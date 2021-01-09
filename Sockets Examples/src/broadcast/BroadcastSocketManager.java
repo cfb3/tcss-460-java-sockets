2170b40356b94f5ebaffaea066b1053a02d54b98
charles <cfb3@uw.edu>
2021-01-09 08:09:04 -0800

    Updated BroadcastSocetManager - removed unused List<Socket> + added method to send a message to an individual socket.

--- Sockets Examples/src/broadcast/BroadcastSocketManager.java
+++ Sockets Examples/src/broadcast/BroadcastSocketManager.java
@@ -3,32 +3,26 @@ package broadcast;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.Socket;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 public class BroadcastSocketManager {
 
-    private final List<Socket> mySockets;
     private final Map<Socket, PrintWriter> myWriters;
     
     public BroadcastSocketManager() {
         super();
-        mySockets = new ArrayList<>();
         myWriters = new HashMap<>();
     }
     
     public synchronized void addSocket(final Socket theSocket) throws IOException {
-        if (mySockets.contains(theSocket)) {
+        if (myWriters.keySet().contains(theSocket)) {
             throw new IllegalArgumentException("I already manage this Socket");
         }
-        mySockets.add(theSocket);
         myWriters.put(theSocket, new PrintWriter(theSocket.getOutputStream(), true));
     }
     
     public synchronized void removeSocket(final Socket theSocket) {
-        mySockets.remove(theSocket);
         myWriters.remove(theSocket);
     }
     
@@ -36,5 +30,9 @@ public class BroadcastSocketManager {
         myWriters.entrySet().stream()
             .filter(s -> !s.getKey().isClosed())
             .forEach(set -> set.getValue().println(theMessage));
+    }
+    
+    public synchronized void sendMessage(final String theMessage, final Socket theDestination) {
+        myWriters.get(theDestination).println(theMessage);
     }
 }
